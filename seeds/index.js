1f99e7436bddadd3ef447f3d38cf965ba3b8124a
John Hur <hur.john628@gmail.com>
2022-01-17 00:24:01 -0600

    cycleType seed

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const { Apartment, Complex, CycleType, DryerSettings, DryLevel, Employee, Machin
 const casual = require('casual');
 
 const complexData = require('./complexData.json');
-const CycleTypeData = require('./cycleTypeData.json');
+const cycleTypeData = require('./cycleTypeData.json');
 const dryLevelData = require('./dryLevelData.json');
 const employeeData = require('./employeeData.json');
 const machineData = require('./machineData.json');
@@ -13,27 +13,32 @@ const roleData = require('./roleData.json');
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    let userArray = [];
-
-    for (let i = 0; i < 3; i++) {
-        let user = {
-            "apartment_id": 1 + i,
-            "firstname": casual.first_name,
-            "lastname": casual.last_name,
-            "phone_number": casual.phone,
-            "email": casual.email,
-            "password": `password${i}`
-        };
-        userArray.push(user);
-    }
-
-    console.log("User Array:")
-    console.log(userArray);
-
-    const users = await User.bulkCreate(userArray, {
+    const cycleType = await CycleType.bulkCreate(cycleTypeData, {
         individualHooks: true,
         return: true,
     });
+
+    // let userArray = [];
+
+    // for (let i = 0; i < 3; i++) {
+    //     let user = {
+    //         "apartment_id": 1 + i,
+    //         "firstname": casual.first_name,
+    //         "lastname": casual.last_name,
+    //         "phone_number": casual.phone,
+    //         "email": casual.email,
+    //         "password": `password${i}`
+    //     };
+    //     userArray.push(user);
+    // }
+
+    // console.log("User Array:")
+    // console.log(userArray);
+
+    // const users = await User.bulkCreate(userArray, {
+    //     individualHooks: true,
+    //     return: true,
+    // });
 
     process.exit(0);
 };
