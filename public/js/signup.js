b6e9d7df57f032abe73464f624a231441a96ac1e
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-01-17 18:35:24 -0800

    added signup js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,20 +1,21 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const firstName = document.querySelector('#first-name-signup').value.trim();
-  const lastName = document.querySelector('#last-name-signup').value.trim();
+  const apartment_id = document.querySelector('#apartment-signup').value.trim();
+  const firstname = document.querySelector('#first-name-signup').value.trim();
+  const lastname = document.querySelector('#last-name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
-  const phone = document.querySelector('#phone-signup').value.trim();
+  const phone_number = document.querySelector('#phone-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if(firstName && lastName && email && phone && password){
+  if (apartment_id && firstname && lastname && email && phone_number && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({name, email, password }),
-      headers: {'Content-Type': 'application/json' },
+      body: JSON.stringify({ apartment_id, firstname, lastname, phone_number, email, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
-    if(response.ok) {
+    if (response.ok) {
       document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
