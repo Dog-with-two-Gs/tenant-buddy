49597239584bfaefc786168a9586f252df3b91cd
John Hur <hur.john628@gmail.com>
2022-01-19 06:14:14 -0600

    reserve now frontend js

--- controllers/api/reservationRoutes.js
+++ controllers/api/reservationRoutes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
+const{ Reservation } = require('../../models');
+const isAuth = require('../../utils/auth')
 
 // Make GET, CREATE, PUT, DELETE routes
+
+router.post('/', isAuth, async (req, res) => {
+    try {
+        const reservationData = await Reservation.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    };
+});
 
 module.exports = router;
--- public/js/reservation.js
+++ public/js/reservation.js
@@ -5,3 +5,32 @@ var weekTime = moment(currentTime).add(7, 'days').format("YYYY-MM-DDThh:mm");
 
 reserveTime.setAttribute('min', `${currentTime}`);
 reserveTime.setAttribute('max', `${weekTime}`);
+
+const reserveNowHandler = async (event) => {
+    event.preventDefault();
+
+    const machine_id = document.querySelector('#hiddenId');
+    const created_at = currentTime;
+    const started_at = currentTime;
+    const reserve_time = currentTime;
+
+    if (machine_id && created_at && started_at && reserve_time) {
+        const response = await fetch(`/api/reservation`, {
+            method: 'POST',
+            body: JSON.stringify({machine_id, created_at, started_at, reserve_time}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create reservation.')
+        };
+    };
+};
+
+document
+  .querySelector('.available-machine-form')
+  .addEventListener('submit', reserveNowHandler)
