d1ebb8ec8a49d3090f68360b6c9ee9c3fff614c6
John Hur <hur.john628@gmail.com>
2022-01-20 19:16:18 -0600

    changes status id upon reservation

--- controllers/api/machineRoutes.js
+++ controllers/api/machineRoutes.js
@@ -1,4 +1,30 @@
 const router = require('express').Router();
+const { Machine } = require('../../models');
+const isAuth = require('../../utils/auth')
+
+// PUT route for status_id
+router.put('/:id', isAuth, async (req, res) => {
+    try {
+        const machineData = await Machine.update(
+            {
+                status_id: 2,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            },
+        );
+
+        if (!machineData) {
+            res.status(404).json({ message: 'No machine found with this id' })
+        };
+
+        res.status(200).json(machineData);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
 
 // Make GET route
 
